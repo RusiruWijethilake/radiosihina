80bee57ef194de18f3bc4839ebdc3b56680a7064
Rusiru Wijethilake <rusiru@ascentic.se>
2022-06-07 22:54:08 +0530

    dashboard update and library fix

--- public/js/library.js
+++ public/js/library.js
@@ -1,3 +1,5 @@
+startSpinner();
+
 const firebaseConfig = {
     apiKey: "AIzaSyANGR2PoeFAXlEm0VAnLHPCoyrvq6L6T1Q",
     authDomain: "radio-sihina.firebaseapp.com",
@@ -22,14 +24,22 @@ db.collection("programs")
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             var newProgram = document.createElement("option");
+            var url = new URL(document.URL);
+            var urlproid = url.searchParams.get("proid");
 
             db.collection("presenters").doc(doc.data().presenter).get().then((presenter) => {
               newProgram.innerHTML = doc.data().name+" by "+presenter.data().firstname+" "+presenter.data().lastname;
               newProgram.value = ""+doc.id+"";
+              if(urlproid != null){
+                newProgram.setAttribute("selected", true);
+                showRecordings(newProgram);
+              }
               programList.appendChild(newProgram);
             }).catch((er) => {
               console.log("Error getting document:", er);
             });
+
+            stopSpinner();
         });
     })
     .catch((error) => {
@@ -37,7 +47,9 @@ db.collection("programs")
     });
 
 function showRecordings(selected) {
+  startSpinner();
   var proid = selected.value;
+  var number = 0;
   recordingList.innerHTML = "";
 
   logEvent(proid+" clicked");
@@ -48,11 +60,11 @@ function showRecordings(selected) {
       .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
               var id = doc.data().id;
-              var number = doc.data().number;
               var link = doc.data().link;
               var dat = doc.data().date.toDate().toDateString() + " " + doc.data().date.toDate().toLocaleTimeString('en-US');
               db.collection("programs").doc(id).get().then((program) => {
                 db.collection("presenters").doc(program.data().presenter).get().then((presenter) => {
+                  number = number + 1;
                   var by = presenter.data().firstname + " " + presenter.data().lastname;
 
                   var newDesc = document.createElement("tr");
@@ -69,16 +81,37 @@ function showRecordings(selected) {
               }).catch((error) => {
                 console.log("Error getting document:", error);
               });
+
+              stopSpinner();
           });
+          var url = new URL(document.URL);
+          url.searchParams.set('proid', proid);
+          const state = { 'user': 'guest' };
+          const title = 'Radio Sihina program';
+          history.pushState(state, title, url.toString());
       })
       .catch((error) => {
           console.log("Error getting documents: ", error);
       });
   }else{
     recordingList.innerHTML = "";
+    var url = new URL(document.URL);
+    url.searchParams.delete('proid');
+    const state = { 'user': 'guest' };
+    const title = 'Radio Sihina program';
+    history.pushState(state, title, url.toString());
+    stopSpinner();
   }
 }
 
 function logEvent(message){
   analytics.logEvent("LIBRARY "+message);
+}
+
+function startSpinner(){
+    document.getElementById("overlay").style.display = "block";
+}
+
+function stopSpinner(){
+    document.getElementById("overlay").style.display = "none";
 }
--- public/presenter/js/presenter-dashboard.js
+++ public/presenter/js/presenter-dashboard.js
@@ -1,3 +1,5 @@
+startSpinner();
+
 const firebaseConfig = {
     apiKey: "AIzaSyANGR2PoeFAXlEm0VAnLHPCoyrvq6L6T1Q",
     authDomain: "radio-sihina.firebaseapp.com",
@@ -14,8 +16,17 @@ const auth = firebase.auth();
 const analytics = firebase.analytics();
 const user = auth.currentUser;
 const db = firebase.firestore();
+const storage = firebase.storage();
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 var uid = "";
+var selectDate = 0;
+var selectTime = new Date();
+var selectProId = "";
+var selectProName = "";
+var daysInAWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 auth.onAuthStateChanged((user) => {
     if (user) {
@@ -29,6 +40,8 @@ auth.onAuthStateChanged((user) => {
         }
 
         uid = user.uid;
+
+        document.getElementById("txtUserId").innerHTML = uid;
 
         db.collection("programs").where("presenter", "==", uid)
         .get()
@@ -50,6 +63,7 @@ auth.onAuthStateChanged((user) => {
         .catch((error) => {
             console.log("Error getting documents: ", error);
         });
+        stopSpinner();
     } else {
         location.replace("index.html");
     }
@@ -57,12 +71,15 @@ auth.onAuthStateChanged((user) => {
 
 function changeUserDisplayName(){
     var newDisplayName = prompt("Enter your first and last name (Eg: John Doe)");
+    startSpinner();
     firebase.auth().currentUser.updateProfile({
         displayName: newDisplayName,
       }).then(() => {
         alert("Name updated successfully!")
+        stopSpinner();
       }).catch((error) => {
         alert(error.message);
+        stopSpinner();
       });
 }
 
@@ -78,6 +95,8 @@ function signOutUser(){
 var programManager = document.getElementById("program-manager-view");
 
 function showProgramManager(selected) {
+    startSpinner();
+
     var proid = selected.value;
     programManager.innerHTML = "";
 
@@ -85,14 +104,153 @@ function showProgramManager(selected) {
         db.collection("programs").doc(proid)
             .get()
             .then((doc) => {
-                console.log(doc.data());
-                var newTableView = document.createElement("table");
-                newTableView.innerHTML = "<tr><td></td></tr>";
+                selectDate = doc.data().day;
+                selectTime = doc.data().schedule.toDate();;
+                selectProId = doc.id;
+                selectProName = doc.data().name;
+
+                storage.refFromURL(doc.data().art).getDownloadURL()
+                .then((url) => {
+                    programManager.innerHTML = "<div class='table-responsive table-responsive-xxl'><table class='table'><tr><td rowspan='5'><img src='"+url+"' alt='program art' class='program-art-image'></td><td colspan='3' class='text-center'><span class='h2'>"+doc.data().name+"</span></td></tr><tr><td class='col-sm-4'><span class='h6'>Scheduled Day</span><br><input class='form-control' type='text' value='"+daysInAWeek[selectDate]+"' disabled></td><td class='col-sm-4'><span class='h6'>Scheduled Time</span><br><input class='form-control' type='text' value='"+formatAMPM(doc.data().schedule.toDate())+"' disabled></td></tr><tr><td colspan='2'><span class='h5'>Upload new program</span></td></tr><tr><td><span class='h6'>Select a date</span><input class='form-control' type='date' onchange='checkSelectedDate(this)' min='"+getMinDate()+"' id='txtDate' required></td><td><span class='h6'>Upload program</span><input class='form-control' type='file' id='txtFile' accept='.mp3,audio/*' required></td></tr><tr><td colspan='2' class='text-center'><button class='btn btn-gradiented' onclick='uploadAndSubmit()'>Upload and Publish</button></td></tr></table></div>";
+
+                    stopSpinner();
+                })
+                .catch((error) => {
+                  console.log(error.message);
+                  stopSpinner();
+                });
             })
             .catch((error) => {
                 console.log("Error getting documents: ", error);
+                stopSpinner();
             });
       }else{
-        programManager.innerHTML = "";
+        programManager.innerHTML = "<div class='table-responsive table-responsive-xxl' style='visibility: hidden;'><table class='table'><tr><td rowspan='5'><img src='../img/placeholder.jpg' alt='program art'></td><td colspan='3' class='text-center'><span class='h2'>Program Name</span></td></tr><tr><td class='col-sm-4'><span class='h6'>Scheduled Date </span><br><input class='form-control' type='text' disabled></td><td class='col-sm-4'><span class='h6'>Scheduled Time </span><br><input class='form-control' type='text' disabled></td></tr><tr><td colspan='2'><span class='h5'>Upload new program</span></td></tr><tr><td><span class='h6'>Select a date</span><input class='form-control' type='date' onchange='checkSelectedDate(this)' id='txtDate' required></td><td><span class='h6'>Upload program</span><input class='form-control' type='file' accept='.mp3,audio/*' required></td></tr><tr><td colspan='2' class='text-center'><button class='btn btn-gradiented'>Upload and Publish</button></td></tr></table></div>";
+        stopSpinner();
       }
+}
+
+function checkSelectedDate(element){
+    var day = new Date(element.value).getDay();
+    if(day != selectDate){
+        alert("Invalid program for your date. Select only "+daysInAWeek[selectDate]+"s");
+        element.value = "";
+    }
+}
+
+function getMinDate(){
+    var dtToday = new Date();
+
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+
+    var maxDate = year + '-' + month + '-' + day;
+
+    return maxDate;
+}
+
+function uploadAndSubmit(){
+    if(selectProId != ""){
+        var date = document.getElementById("txtDate").value;
+        var file = document.getElementById("txtFile").files[0];
+        if(date != "" && file != null){
+            var conf = confirm("Are you sure you want to upload");
+            if(conf){
+                startSpinner();
+
+                var newDate = new Date(date+" "+selectTime.toLocaleTimeString());
+                var newTimeStamp = firebase.firestore.Timestamp.fromDate(newDate);
+
+                var alreadyThere = false;
+
+                db.collection("library").where("date", "==", newTimeStamp)
+                .get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        alreadyThere = true;
+                        console.log(doc.id, " => ", doc.data());
+                    });
+
+                    if(!alreadyThere){
+                        stopSpinner();
+                        startUploader();
+
+                        var storageRef = storage.ref().child('programs-recordings/'+selectProName+" - "+date+".mp3");
+                        var task = storageRef.put(file);
+    
+                        task.on('state_changed', 
+                            (snapshot) => {
+                                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                                document.getElementById("progressValue").innerHTML = (snapshot.bytesTransferred / (1024 ** 2)).toFixed(2)+"mb / "+(snapshot.totalBytes / (1024 ** 2)).toFixed(2)+"mb";
+                            }, 
+                            (error) => {
+                                stopSpinner();
+                                alert("File upload failed\n"+error.message);
+                            }, 
+                            () => {
+                                task.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                                    console.log('File available at', downloadURL);
+                                    db.collection("library").add({
+                                        id: selectProId,
+                                        link: downloadURL,
+                                        date: newTimeStamp
+                                    })
+                                    .then((docRef) => {
+                                        stopUploader();
+                                        alert("Created recording successfully! New recording for "+selectProName+" added to "+date);
+                                        document.getElementById("txtDate").value = "";
+                                        document.getElementById("txtFile").value = "";
+                                    })
+                                    .catch((error) => {
+                                        stopSpinner();
+                                        alert("Failed to create the record\n"+error.message);
+                                    });
+                                });
+                            }
+                        );
+                    }else{
+                        stopSpinner();
+                        alert("Record for this date already exists!")
+                    }
+                })
+                .catch((error) => {
+                    console.log("Error getting documents: ", error);
+                });
+            }
+        }else{
+            alert("Please fill all the required fields");
+        }
+    }
+}
+
+const formatAMPM = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes.toString().padStart(2, '0');
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+function startSpinner(){
+    document.getElementById("overlay").style.display = "block";
+}
+
+function stopSpinner(){
+    document.getElementById("overlay").style.display = "none";
+}
+
+function startUploader(){
+    document.getElementById("progress-overlay").style.display = "block";
+}
+
+function stopUploader(){
+    document.getElementById("progress-overlay").style.display = "none";
 }
